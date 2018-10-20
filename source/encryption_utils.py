729e902a263319852de6a96f1368dd7b65e4e8e4
EMaguir1 <Eamon.Maguire1@T-Mobile.com>
2018-10-20 16:41:36 -0700

    AES key initialization working

--- source/encryption_utils.py
+++ source/encryption_utils.py
@@ -1,14 +1,28 @@
 import rsa
+import binascii
+import hashlib
 from Crypto.Cipher import AES
+
 
 class EncryptionUtils:
 
     def __init__(self, aes_key, account_details, bank_public_key=None):
-        self.aes_key = aes_key
+        # convert to byte array in preparation for conversion to hexadecimal
+        key_bytes = bytearray('752EF0D8FB4958670DBA40AB1F3C1D0F8FB4958670DBA40AB1F3752EF0DC1D0F', 'utf8')
+        print('byte array: ' + str(key_bytes))
+        # convert byte array to hexadecimal in preparation of generating a SHA256 digest
+        hex_key = binascii.hexlify(key_bytes)
+        print('hex key string: ' + str(hex_key))
+        # create instance of SHA256
+        h = hashlib.sha256()
+        # hash the hexadecimal representation of the key
+        h.update(hex_key)
+        # generate the digest to serve as the AES key
+        self.aes_key = h.digest()
         self.bank_public_key = bank_public_key
         self.account_details = account_details
         self.bank_public_key = bank_public_key
-        self.aes = AES.new(aes_key, AES.MODE_CBC)
+        self.aes = AES.new(self.aes_key, AES.MODE_ECB)
 
     def set_bank_public_key(self, bank_pu):
         self.bank_public_key = bank_pu
